63e89c1a6244c7b14ce663177b883f28a20a7867
Ansyahmf_ <firdiansyahmf.04@gmail.com>
2026-04-07 14:41:55 +0700

    feat: buat master layout admin dan app menggunakan Blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cireng A'paweh</title>
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
