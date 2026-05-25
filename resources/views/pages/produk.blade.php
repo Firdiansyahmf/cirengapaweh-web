b242ed751d09e64a261fd6c59beda7c34d426b1a
Lyapunobu <anaqiahza2006@gmail.com>
2026-05-25 12:07:11 +0700

    style: merapihkan css detail produk

--- resources/views/pages/produk.blade.php
+++ resources/views/pages/produk.blade.php
@@ -9,65 +9,65 @@
 
 @section("content")
 
-    {{-- @include("components.detail") --}}
-
-    <div class="detail">
+    <div class="detail flexRow">
         <div class="breakpoint">
-
-            <div class="product-visual">
-                <img src="{{ asset('assets/img/produk/Cireng Salju Kuah Keju Creamy.jpg') }}" alt="Cireng Kuah Keju" class="main-product-img">
-            </div>
-
-            <div class="product-info-content">
-                <div class="displayH1 charcoalGrey">
-                    Spesial Buat Kamu: <br>
-                    <span class="primaryBrandRed">Cireng Kuah Keju Juara!</span>
+            
+            <div class="box1 flexRow">
+                <div class="productVisual">
+                    <img src="{{ asset('assets/img/produk/Cireng Salju Kuah Keju Creamy.jpg') }}" alt="Cireng Kuah Keju">
                 </div>
 
-                <div class="displayH2">
-                    Rp<span>15.000</span>
-                </div>
-
-                <div class="seller-message">
-                    <div class="quote-box">
-                        <div class="quote">
-                            <img src="{{ asset('assets/icon/kutip.svg') }}" alt="Quote" class="charcoalGrey">
-                        </div>
-                        <span class="subH4">Pesan dari A'Paweh</span>
+                <div class="flexCol">
+                    <div class="productTitle flexCol">
+                        <h1 class="displayH1 charcoalGrey">
+                            Spesial Buat Kamu: <br>
+                            <span class="primaryBrandRed">Cireng Kuah Keju Juara!</span>
+                        </h1>
+                        <h2 class="displayH2">
+                            Rp<span>15.000</span>
+                        </h2>
                     </div>
-                    <p class="bodyLg">
-                        Halo Jajaners! A'Paweh udah siapin perpaduan kenyalnya aci pilihan dengan siraman kuah keju
-                        lumer yang rahasia. Gak cuma pedas, tapi gurihnya bikin kamu gak bisa berhenti.
-                    </p>
-                </div>
 
-                <div class="social-proof-box">
-                    <img src="{{ asset('assets/icon/fire.svg') }}" alt="Fire" class="fire-icon">
-                    <p class="bodyLg">Terjual Ribuan Porsi Tiap Hari di Kampus!</p>
-                </div>
-
-                <div class="product-buy">
-                    <p class="subH4">Atur Jumlah Pembelian</p>
-                    <form method="POST" action="{{ url('/checkout') }}">
-                        @csrf
-                        <input type="hidden" name="product_name" value="Cireng Kuah Keju Juara!">
-                        <input type="hidden" name="price" value="15000">
-                        <div class="quantity">
-                            <button type="button"><img src="{{ asset('assets/icon/minus.svg') }}" alt="minus"></button>
-                            {{-- quantity input needs a name so it posts --}}
-                            <input name="quantity" type="number" class="subH3 primaryBrandRed" value="1" min="1" max="99">
-                            <button type="button"><img src="{{ asset('assets/icon/plus.svg') }}" alt="plus"></button>
+                    <div class="sellerMsg">
+                        <div class="sellerQuote flexRow">
+                            <img src="{{ asset('assets/icon/kutip.svg') }}" alt="Quote" class="charcoalGrey">
+                            <h4 class="subH4">Pesan dari A'Paweh</h4>
                         </div>
-                        <div class="subtotal">
-                            Subtotal <p class="bodyLg"><b>Rp<span>15.000</span></b></p>
-                        </div>
-                        <button type="submit" class="btnPrimary">Beli Langsung</button>
-                    </form>
-                </div>
-            </div>
+                        <span class="bodyLg">
+                            Halo Jajaners! A'Paweh udah siapin perpaduan kenyalnya aci pilihan dengan siraman kuah keju
+                            lumer yang rahasia. Gak cuma pedas, tapi gurihnya bikin kamu gak bisa berhenti.
+                        </span>
+                    </div>
 
-        </div>
-    </div>
+                    <div class="socialProof">
+                        <img src="{{ asset('assets/icon/fire.svg') }}" alt="Fire" class="fire-icon">
+                        <span class="bodyLg">Terjual Ribuan Porsi Tiap Hari di Kampus!</span>
+                    </div>
+
+                    <div class="productBuy flexCol">
+                        <span class="subH4">Atur Jumlah Pembelian</span>
+                        <form method="POST" action="{{ url('/checkout') }}">
+                            @csrf
+                            <input type="hidden" name="product_name" value="Cireng Kuah Keju Juara!">
+                            <input type="hidden" name="price" value="15000">
+                            <div class="flexCol">
+                                <div class="quantityInput">
+                                    <button type="button"><img src="{{ asset('assets/icon/minus.svg') }}" alt="minus"></button>
+                                    <input name="quantity" type="number" class="subH3 primaryBrandRed" value="1" min="1" max="99">
+                                    <button type="button"><img src="{{ asset('assets/icon/plus.svg') }}" alt="plus"></button>
+                                </div>
+                                <div class="subtotal flexRow bodyMain">
+                                    Subtotal <span class="bodyLg"><b>Rp<span>15.000</span></b></span>
+                                </div>
+                                <button type="submit" class="btnPrimary">Beli Langsung</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div> {{-- end box1 --}}
+
+        </div> {{-- end breakpoint --}}
+    </div> {{-- end detail --}}
 
 @endsection
 
